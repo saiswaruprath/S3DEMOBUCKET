34830e3c62d03a111bbcc7f7bd66e4297896bb79
Sai Swarup Rath <saiswaruprath7@gmail.com>
2023-11-09 21:37:23 -0800

    Add files via upload

---
+++ infra-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3'
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'InfraQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+
+    //S3 bucket
+
+    const s3demobucket =  new s3.Bucket(this, 's3demobucket0122', {
+      bucketName: 'newbucket4567',
+      versioned: false,
+      publicReadAccess: false
+    })
+  }
+}
